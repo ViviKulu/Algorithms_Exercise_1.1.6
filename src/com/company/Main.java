c57cca2fcc719ae682c2a1b38e2a11b699627979
Vivian Babirye <babimaji1111@gmail.com>
2018-12-19 23:05:11 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    //    What does the following program print?
+    //    int f = 0;
+    //    int g = 1;
+    //
+    //    for (int i = 0; i <= 15; i++){
+    //        StdOut.println(f);
+    //        f = f + g;
+    //        g = f - g;
+    //    }
+
+    //    Answer:
+
+    //    0
+    //    1
+    //    1
+    //    2
+    //    3
+    //    5
+    //    8
+    //    13
+    //    21
+    //    34
+    //    55
+    //    89
+    //    144
+    //    233
+    //    377
+    //    610
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
